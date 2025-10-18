d66157b7d873245789073d3a1c25f91064949d98
thnguyen290106 <thnguyen290106@gmail.com>
2025-10-18 16:34:46 +0700

    Installed SQLite extension

--- DoAn/ChessGame/obj/Debug/net8.0-windows/ChessGame.AssemblyInfo.cs
+++ DoAn/ChessGame/obj/Debug/net8.0-windows/ChessGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ae3f27a3bb1a0ef989af7bfd7e8b7906c026d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28950efc46d0eed6d07636528b0e264e4f604cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
