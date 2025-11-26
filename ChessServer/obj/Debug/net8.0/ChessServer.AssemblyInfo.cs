604d0a0417a35c873313918bd5cd338761bdd876
thnguyen290106 <thnguyen290106@gmail.com>
2025-11-26 11:08:23 +0700

    Removed unnecessary files

--- ChessServer/obj/Debug/net8.0/ChessServer.AssemblyInfo.cs
+++ ChessServer/obj/Debug/net8.0/ChessServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e36667eb5db98b1a3d834da5d6897524b8c706e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
